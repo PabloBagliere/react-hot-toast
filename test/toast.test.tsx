89da1c606348db855762a3a49600fabadd6d77e8
Pablo Bagliere <pablo@mundoit.com.ar>
2022-01-20 09:29:12 -0300

    change name

--- test/toast.test.tsx
+++ test/toast.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Toaster, toast } from '../src';
+import { Toaster } from '../component';
+import { toast } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
